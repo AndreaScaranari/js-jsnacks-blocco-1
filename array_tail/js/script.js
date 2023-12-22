271adc7f3331a5e2ef4988b7f6c63ae3fcbd377a
AndreaScaranari <andrea.scaranari.93@gmail.com>
2023-12-22 17:48:12 +0100

    esercizio_2_completed

---
+++ array_tail/js/script.js
@@ -0,0 +1,27 @@
+// Creiamo un array chiedendo all'utente quanti elementi dovrà contenere
+const nElementi = parseInt(prompt("Quanti numeri casuali vuoi che io generi?"));
+const aElementi = [];
+console.log(nElementi);
+const vElementi = parseInt(prompt("Quanti numeri casuali di quelli generati vuoi che ti mostri?"));
+
+// validation
+if (isNaN(nElementi) || isNaN(vElementi)) {
+    alert("Inserisci valori numerici!");
+} else if (nElementi <= 0 || vElementi <= 0) {
+    alert("I numeri inseriti devono essere superiori a 0!");
+} else if (nElementi < vElementi) {
+    alert("I numeri da visualizzare non possono essere più di quelli generati!")
+}
+
+// generando tanti numeri casuali (compresi nell'intervallo da 1 a 100) per quanti sono gli elementi da inserire
+for (let i = 0; i < nElementi; i++) {
+    aElementi[i] = Math.floor(Math.random()*100)+1;
+};
+console.log("I numeri casuali generati sono i seguenti: " + aElementi);
+
+// Stampiamo poi gli elementi dell'Array
+let lastElements = [];
+for (let i = 1; i <= vElementi && i <= nElementi; i++) {
+    lastElements.push(aElementi[aElementi.length-i]);
+}
+console.table(`Gli ultimi ${vElementi} numeri casuali generati sono: ${lastElements}`);
